ae4a0bafeca718b81d119795d52f3ec3d887d99d
ankit-nag01 <ankit01@nagarro.com>
2024-09-18 15:54:19 +0530

    for testing code

--- blocks/bannerCard/bannerCard.js
+++ blocks/bannerCard/bannerCard.js
@@ -16,19 +16,11 @@ export default function decorate(block) {
 
   const BannerHTML = `
   <div class="main-container">
-  <div class="banner-container">
-    <div class="banner-image">
-    <img src="${imageBanner}" alt="${imageAlt}">
-      <div class="banner-content">
+
         <div class="bannerCard-title">
         ${imageTitle}
         </div>
-        <div class="bannerCard-description">
-        ${imageDescription}
-        </div>
-      </div>
-    </div>
-  </div>
+        
 </div>
   `;
 
