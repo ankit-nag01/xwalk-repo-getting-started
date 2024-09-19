b91ced3d047bdcc001a79d5722935d1ee80a7241
ankit-nag01 <ankit01@nagarro.com>
2024-09-19 10:46:42 +0530

    updted for testing image field

--- blocks/bannerCard/bannerCard.js
+++ blocks/bannerCard/bannerCard.js
@@ -1,24 +1,16 @@
 export default function decorate(block) {
     const[
       imageEl,
-      altTextEl,
-      titleEl,
-      descriptionEl
     ] = block.children
 
     const imageElement = imageEl.querySelector('img');
     const imageBanner = imageElement?.getAttribute('src')?.trim() || "";
 
-    const imageAlt = altTextEl?.textContent?.trim() || "";
-    const imageTitle = titleEl?.textContent?.trim() || "";
-    const imageDescription = descriptionEl?.textContent?.trim() || "";
-
-
   const BannerHTML = `
   <div class="main-container">
 
         <div class="bannerCard-title">
-        ${imageTitle}
+        <img src="${imageBanner}" alt="${imageAlt}">
         </div>
         
 </div>
