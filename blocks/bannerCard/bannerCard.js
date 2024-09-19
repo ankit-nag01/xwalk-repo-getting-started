305807e2281a88bb105c70747258e72bc9e6a0c6
ankit-nag01 <ankit01@nagarro.com>
2024-09-19 10:50:30 +0530

    for testing image and title

--- blocks/bannerCard/bannerCard.js
+++ blocks/bannerCard/bannerCard.js
@@ -1,16 +1,20 @@
 export default function decorate(block) {
     const[
       imageEl,
+      titleEl
     ] = block.children
 
     const imageElement = imageEl.querySelector('img');
     const imageBanner = imageElement?.getAttribute('src')?.trim() || "";
+
+    const imageTitle = titleEl?.textContent?.trim() || "";
 
   const BannerHTML = `
   <div class="main-container">
 
         <div class="bannerCard-title">
         <img src="${imageBanner}" alt="${imageAlt}">
+        ${imageTitle}
         </div>
         
 </div>
